4bbc2a91bd136c61faece5fb07a6f6b8b5396fc5
Averand1 <leonk8867@gmail.com>
2022-02-18 22:21:48 +0530

    Spelling correction

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -32,7 +32,7 @@ const Welcome = () => {
                         onClick={connectWallet}
                         className = "flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[2546bd]"
                     >
-                        <p className="text-white text-base font-semibold">Connect Walllet</p>
+                        <p className="text-white text-base font-semibold">Connect Wallet</p>
 
                     </button>
                 </div>
